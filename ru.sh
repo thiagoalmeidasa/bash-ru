9f825a4488be33a71fab862b4002c4c80fb34ef9
Thiago Almeida <thiagoalmeidasa@gmail.com>
2016-01-06 11:47:00 -0300

    "Almoço" added.
    Working on some improvements, like identify lunch and dinner patterns.

--- ru.sh
+++ ru.sh
@@ -13,13 +13,22 @@ case $weekday in
         ;;
     5 ) begin="Sexta"; end=" "
         ;;
-    * ) echo -n "O RU só funciona em dias de semana!"
+    * ) echo -e "O RU só funciona em dias de semana!"
         exit
         ;;
 esac
 
 function getcru () {
-    curl -s 'http://ru.ufpa.br/index.php?option=com_content&view=article&id=7' | sed -e 's/<[^>]*>//g; s/[[^]]*]//g; s/  / /g; /^$/d;/^[[:space:]]*$/d;/Segunda/,/Card/! d; /'"$begin"'/,/'"$end"'/!d; /'"$end"'/d; s/^[ \t]*//  # s/ *-/Almoço\n/1; s/-/\n/2g;'
+    curl -s 'http://ru.ufpa.br/index.php?option=com_content&view=article&id=7'\
+       | sed -e 's/<[^>]*>//g; s/[[^]]*]//g;
+       s/  / /g; /^$/d;
+       /^[[:space:]]*$/d;
+       /Segunda/,/Card/! d;
+       /'"$begin"'/,/'"$end"'/!d; /'"$end"'/d;
+       /'"$begin"'/a\Almoço\n'
+       #/\([A-Z]\{2,\}\)/i\\nJANTAR\n #working on
+      # s/^[ \t]*//  # s/ *-/Almoço\n/1;' #no more needed.
+      # s/-/\n/2g;' #no more needed.
 }
 
 getcru ${begin} ${end}
